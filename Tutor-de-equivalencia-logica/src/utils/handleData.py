92575200e4c7c328f67a183e364c3edbd5b480d4
davi <davimelovas61@gmail.com>
2025-06-30 08:29:19 -0300

    WIP: Leitura de arquivos praticamente feita

--- Tutor-de-equivalencia-logica/src/utils/handleData.py
+++ Tutor-de-equivalencia-logica/src/utils/handleData.py
@@ -1,4 +1,10 @@
 from loadData import loadData
+from calculoAcertos import calculoAcertos
 
-dados = loadData()
+nome, questoesNivelUm, questoesNivelDois, questoesNivelTres = loadData()
 
+acertosNivelUm, quantidadeAcertosNivelUm = calculoAcertos(questoesNivelUm)
+
+acertosNivelDois, quantidadeAcertosNivelDois = calculoAcertos(questoesNivelDois)
+
+acertosNivelTres, quantidadeAcertosNivelTres = calculoAcertos(questoesNivelTres)
--- Tutor-de-equivalencia-logica/src/utils/loadData.py
+++ Tutor-de-equivalencia-logica/src/utils/loadData.py
@@ -4,4 +4,13 @@ def loadData():
     with open('../../dados/dados.json', 'r', encoding='utf-8') as arquivo:
         dados = json.load
 
-    return dados
+    with open('../../questoes/nivel1.json', 'r', encoding='utf-8') as nivelUm:
+        questoesNivelUm = nivelUm.load
+
+    with open('../../questoes/nivel2.json', 'r', encoding='utf-8') as nivelDois:
+        questoesNivelDois = nivelDois.load
+
+    with open('../../questoes/nivel3.json', 'r', encoding='utf-8') as nivelTres:
+        questoesNivelTres = nivelTres.load
+    
+    return dados, questoesNivelUm, questoesNivelDois, questoesNivelTres
